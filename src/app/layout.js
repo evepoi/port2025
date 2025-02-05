1a52b697815338310f1ef880c9fba55b882356b4
evepoi <55119879+evepoi@users.noreply.github.com>
2025-02-05 13:30:39 +0900

    컨퍼넌트 설정완료

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import "@/assets/scss/style.scss"
+
 export const metadata = {
   title: "next.js 사이트 만들기",
   description: "next.js로 사이트 만드는 연습을 진행하는 공간입니다. 자료가 수시로 바뀔 수 있습니다.",
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,24 @@
+"use client";
+import React, { useEffect } from "react";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Intro from "@/components/Intro";
+import Port from "@/components/Port";
+import Site from "@/components/Site";
+import Skill from "@/components/Skill";
+import Skip from "@/components/Skip";
 import Image from "next/image";
+import lenis from "@/utils/lenis";
+import link from "@/utils/link";
 
 export default function Home() {
+    useEffect(() => {
+        lenis();
+        link();
+
+    }, []);
+    
     return (
         <>
             <>
@@ -13,7 +31,16 @@ export default function Home() {
                         priority
                 />
             </>
-            <main></main>
+            <Skip />
+            <Header />
+            <main id="main" role="main">
+                <Intro />
+                <Skill />
+                <Site />
+                <Port />
+                <Contact />
+            </main>
+            <Footer />
         </>
     );
 }
